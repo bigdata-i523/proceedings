52a6b52db9fb493d5d84355a5b35905260b10e31
Gregor von Laszewski <laszewski@gmail.com>
2017-09-18 10:08:56 -0400

    new:dev: add pull of proceedings

---
+++ pull.py
@@ -0,0 +1,15 @@
+import os
+
+# do this in the shell git clone
+# curl https://api.github.com/orgs/bigdata-i523/repos?per_page=200 | fgrep ssh_url > list.txt
+
+with open('list.txt', 'r') as content_file:
+    content = content_file.readlines()
+
+print (content)
+
+for line in content:
+    url = line.split('"')[3]
+    os.system("git pull " + url)
+
+
