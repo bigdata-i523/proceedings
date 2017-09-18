ce6ba4b60fd9ccbecfb1472c55af72eca1830a70
Gregor von Laszewski <laszewski@gmail.com>
2017-09-18 10:38:53 -0400

    new:dev: add proceedings api template

---
+++ cloudmesh.proceedings/cloudmesh/proceedings/api/proceedings.py
@@ -0,0 +1,21 @@
+class Proceedings(object):
+
+    def get_hids_from_git(self):
+        """ returns the hids from git"""
+        return []
+
+    def get_hids_from_list(self, parameter):
+        """use cloudmesh Parameter to ggenerate a list form abreviated list string"""
+        """example hid[100-110,116,201-210,301-305]"""
+        return
+    
+    def clone(self):
+        """ clones all hid dirs int .."""
+        """returns all hids that have an issue"""
+        return
+    
+    def pull(self):
+        """does a git pull in all hid dirs in .."""
+        """returns all hid the have an issue"""
+        return
+        
